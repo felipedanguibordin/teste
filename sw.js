5fa5a9268ea0ac9737102a0b01761475569c1ab1
felipe.bordin <felipe.bordin@xpert.com.br>
2025-06-09 11:15:34 -0300

    ..

--- sw.js
+++ sw.js
@@ -1,21 +1,20 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("treinos-v2").then((cache) => {
+    caches.open("treinos-v3").then((cache) => {
       return cache.addAll([
         "/",
         "/index.html",
-        "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css",
+        "https://cdn.jsdelivr.net/npm/tailwindcss@3.4.1/dist/tailwind.min.css",
         "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css",
         "https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.min.js",
-        "https://unpkg.com/chart.js@4.4.0/dist/chart.min.js",
-        "https://cdn.jsdelivr.net/npm/js-confetti@0.11.0/dist/js-confetti.browser.js",
+        "https://cdn.jsdelivr.net/npm/js-confetti@0.11.0/dist/js-confetti.min.js",
       ]);
     })
   );
 });
 
 self.addEventListener("activate", (event) => {
-  const cacheWhitelist = ["treinos-v2"];
+  const cacheWhitelist = ["treinos-v3"];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
@@ -35,7 +34,7 @@ self.addEventListener("fetch", (event) => {
       return (
         response ||
         fetch(event.request).catch(() => {
-          console.error("Fetch failed for:", event.request.url);
+          console.warn("Fetch failed for:", event.request.url);
         })
       );
     })
